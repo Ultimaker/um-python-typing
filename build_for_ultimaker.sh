philippe0206/um-pyrsistent

buildscript: Initial version of the buildscript
This build script checks creates a debian package, but does not do this
cross-platform yet. For this we need to create a source package first
and dpkg-buildpackage it in a target-like environment.

CL-46

Signed-off-by: Olliver Schinagl <o.schinagl@ultimaker.com>

build_for_ultimaker.sh
@@ -1,0 +1,18 @@
+#!/bin/sh
+
+SRC_PATH="$(pwd)"
+MODULE_PATH="$(pwd)/pyrsistent"
+
+init() {
+	git submodule update --init --recursive
+}
+
+build() {
+	cd "${MODULE_PATH}" &&
+	python3 setup.py --command-packages=stdeb.command bdist_deb &&
+	cp deb_dist/python3-pyrsistent_*.deb "${SRC_PATH}/"
+	cd "${SRC_PATH}"
+}
+
+init
+build

Ultimaker/um-python-typing

build_for_ultimaker.sh
@@ -1,0 +1,18 @@
+#!/bin/sh
+
+SRC_PATH="$(pwd)"
+MODULE_PATH="$(pwd)/typing"
+
+init() {
+	git submodule update --init --recursive
+}
+
+build() {
+	cd "${MODULE_PATH}" &&
+	python3 setup.py --command-packages=stdeb.command bdist_deb &&
+	cp deb_dist/python3-typing_*.deb "${SRC_PATH}/"
+	cd "${SRC_PATH}"
+}
+
+init
+build